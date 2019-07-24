29362b21a6ad87a5171fa9cd68db86bd711d800c
Matei Eduard <matei.eduard@radcom.ro>
2019-07-24 16:18:44 +0300

    adding angular universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
 import * as RecipeActions from './recipes/store/recipe.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,12 @@ import * as RecipeActions from './recipes/store/recipe.actions';
 export class AppComponent implements OnInit {
   title = 'recipe-project';
 
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(private store: Store<fromApp.AppState>,@Inject(PLATFORM_ID) private platformId) {}
 
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 }
